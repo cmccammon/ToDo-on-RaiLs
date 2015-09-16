79c1acd5bb1818e0d5e17b346c3863f4e509eea7
Chris McCammon <cmccammon@gmail.com>
2015-09-16 18:08:31 -0500

    got normal mode done

--- app/controllers/honeydos_controller.rb
+++ app/controllers/honeydos_controller.rb
@@ -19,4 +19,31 @@ class HoneydosController < ApplicationController
       render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
     end
   end
+
+  def create
+    #For create to work, we must *require* that a parameter is sent with the name 'project'
+    if params[:project].nil? || params[:project].empty?
+      err_msg = "The 'project' parameter was empty or not found"
+      render json: { error_msg: err_msg }.to_json, status: 422
+    else
+      # tweet = Tweet.create(project: params[:project])
+      task = Honeydo.new
+      task.project = params[:project]
+      task.save
+      render json: task.to_json, status: 201
+    end
+  end
+
+  def destroy
+    if Honeydo.exists?(params[:id])
+      task = Honeydo.find(params[:id])
+      task.destroy
+      render json: { message: "Project Deleted Successfully." }, status: 200
+    else
+      render json: { error_msg: 'No project found with that ID', id: params[:id] }.to_json, status: 404
+    end
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get('/honeydos',      { to: 'honeydos#index' })
-  get('/honeydos/new', { to: 'honeydos#new' })
-  get('/honeydos/:id',  { to: 'honeydos#show' })
+  get    '/honeydos',     to: 'honeydos#index'
+  get    '/honeydos/new', to: 'honeydos#new'
+  get    '/honeydos/:id', to: 'honeydos#show'
+  post   '/honeydos',     to: 'honeydos#create'
+  put    '/honeydos/:id', to: 'honeydos#update'
+  delete '/honeydos/:id', to: 'honeydos#destroy'
 end
